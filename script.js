60caad186c32d90e606a50d882740471bf8c20b3
Muhammad Naqib Muhtadi <naiibmhtd@gmail.com>
2025-05-17 09:16:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const target = document.getElementById(targetId);
+    window.scrollTo({
+      top: target.offsetTop - 60,
+      behavior: "smooth"
+    });
+  });
+});
